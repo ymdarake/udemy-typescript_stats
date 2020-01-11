5849e19aa33bdcfafe8c09f56a29490a5c2a9ae2
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 12:48:29 +1300

    [105]Extracting Csv Reading

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hi there!');
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+for (let match of reader.data) {
+  console.log(match);
+}
+
+// for (let match of matches)

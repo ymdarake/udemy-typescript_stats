3a0c45a61f42c42f9aed54ded5d5d4b8d5a87364
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 14:44:28 +1300

    [130-131]Generating instances with Static methods.(decoupling index.ts from dependencies)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { MatchReader } from './MatchReader';
-import { CsvFileReader } from './CsvFileReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
-import { HtmlReport } from './reportTargets/HtmlReport';
 
-const reader = new MatchReader(new CsvFileReader('football.csv'));
+const reader = MatchReader.fromCsv('football.csv');
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
+
 reader.load();
-
-const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
-
 summary.buildAndPrintReport(reader.matches);

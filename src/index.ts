658a3ecda0ccd78c28e06beceda3308b2c7aa40c
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-12 14:39:23 +1300

    [129]Generating HTML Report

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const reader = new MatchReader(new CsvFileReader('football.csv'));
 reader.load();
 
-const summary = new Summary(
-  new WinsAnalysis('Man United'),
-  new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
 
 summary.buildAndPrintReport(reader.matches);
